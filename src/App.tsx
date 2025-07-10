546fdb3b3a341d67b27b67834b2ba982f16271b0
DevGisoun <devgisoun@gmail.com>
2025-07-10 14:42:31 +0900

    feat: useState를 활용한 카운터 및 인풋 상태 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+    // useState => Hooks
+    // useState는 가장 기본적인 Hook. 함수 컴포넌트에서도 가변적인 상태를 지닐 수 있게 해준다.
+    // useState 가 호출되면 배열은 반환한다. => 첫 번째 원소: 상태 값, 두 번째 원소: 상태 설정 함수.
+    // useState 함수의 파라미터에는 상태의 기본값을 넣는다.
+    const [value, setValue] = useState<number>(0);
+    const [name, setName] = useState<string>('이해찬');
+    const [nickname, setNickname] = useState<string>('기순');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const increment = () => setValue(value + 1);
+    const decrement = () => setValue(value - 1);
+
+    const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) =>
+        setName(e.target.value);
+    const onChangeNickame = (e: React.ChangeEvent<HTMLInputElement>) =>
+        setNickname(e.target.value);
+
+    return (
+        <>
+            <div>
+                <p>
+                    현재 카운터: <b>{value}</b>
+                </p>
+
+                <button onClick={increment}>+</button>
+                <button onClick={decrement}>-</button>
+
+                <div>
+                    <input type="text" value={name} onChange={onChangeName} />
+                    <input
+                        type="text"
+                        value={nickname}
+                        onChange={onChangeNickame}
+                    />
+                </div>
+
+                <div>
+                    <b>이름: {name}</b>
+                </div>
+                <div>
+                    <b>닉네임: {nickname}</b>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
